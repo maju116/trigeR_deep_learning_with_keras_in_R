720e3a3a41514fbf59452b91f638f0c50a46ce85
maju116 <michalmaj116@gmail.com>
2020-01-13 22:40:33 +0100

    Update packages

--- PACKAGES.R
+++ PACKAGES.R
@@ -1,2 +1,2 @@
-install.packages(c("tidyverse", "keras", "reticulate", "gridExtra", "rmarkdown"))
+install.packages(c("tidyverse", "keras", "reticulate", "gridExtra", "rmarkdown", "tfruns"))
 devtools::install_github("andrie/deepviz")
