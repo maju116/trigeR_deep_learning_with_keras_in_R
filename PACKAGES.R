bc11b784da2fefa4e067c2ab0ac5a9c8357e48c1
maju116 <michalmaj116@gmail.com>
2020-01-27 21:39:22 +0100

    Add SGD notebook

--- PACKAGES.R
+++ PACKAGES.R
@@ -1,2 +1,2 @@
-install.packages(c("tidyverse", "keras", "reticulate", "gridExtra", "rmarkdown", "tfruns"))
+install.packages(c("tidyverse", "keras", "reticulate", "gridExtra", "rmarkdown", "tfruns", "KODAMA"))
 devtools::install_github("andrie/deepviz")
